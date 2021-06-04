94173829e8175146b953cab5aa67370239da37eb
Olga Mujinga <monaolgita@gmail.com>
2021-06-04 20:34:36 +0200

    refactor:change de librairie

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,8 @@
 
-let element = document.getElementById("section-un-bienvenu");
+// let element = document.getElementById("section-un-bienvenu");
 
-element.className = "slidein";
-function listener(event) {
-    document.getElementById("output")
-  }
+// element.className = "slidein";
+// function listener(event) {
+//     document.getElementById("output")
+//   }
   
